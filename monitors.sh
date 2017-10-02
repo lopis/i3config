af067fd510113598c344c53c27363fa053f6ecff
Joao Lopes <joao.lopes@simplesurance.de>
2017-10-02 16:29:36 +0200

    Added monitor mode picker

--- monitors.sh
+++ monitors.sh
@@ -13,9 +13,14 @@ INTERNAL_POS="0x0"
 EXTERNAL_POS="-320x-1440"
 # xrandr --output $EXTERNAL_OUTPUT --auto --pos $EXTERNAL_POS --output $INTERNAL_OUTPUT --auto --pos $INTERNAL_POS
 
+echo arg1 is $1
 
+# read mode from argument
+if [ ! -z "$1" ] ; then
+  monitor_mode="$1"
+
+elif [ ! -f "/tmp/monitor_mode.dat" ] ; then
 # if we don't have a file, start at zero
-if [ ! -f "/tmp/monitor_mode.dat" ] ; then
   monitor_mode="all"
 
 # otherwise read the value from the file
@@ -23,18 +28,22 @@ else
   monitor_mode=`cat /tmp/monitor_mode.dat`
 fi
 
-if [ $monitor_mode = "all" ]; then
-  monitor_mode="EXTERNAL"
+echo monitor mode is $monitor_mode
+
+
+# Set screen mode
+if [ $monitor_mode = "external" ]; then
   xrandr --output $INTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT --auto
-elif [ $monitor_mode = "EXTERNAL" ]; then
-  monitor_mode="INTERNAL"
+  monitor_mode="internal"
+elif [ $monitor_mode = "internal" ]; then
   xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --off
-elif [ $monitor_mode = "INTERNAL" ]; then
-  monitor_mode="CLONES"
+  monitor_mode="mirror"
+elif [ $monitor_mode = "mirror" ]; then
   xrandr --output $INTERNAL_OUTPUT --auto --output $EXTERNAL_OUTPUT --auto --same-as $INTERNAL_OUTPUT
-else
   monitor_mode="all"
+elif [ $monitor_mode = "all" ]; then
   xrandr --output $EXTERNAL_OUTPUT --auto --pos $EXTERNAL_POS --output $INTERNAL_OUTPUT --auto --pos $INTERNAL_POS
+  monitor_mode="external"
 fi
 
 echo "${monitor_mode}" > /tmp/monitor_mode.dat
